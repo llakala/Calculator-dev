b105f475c65da4eec53e5c3311968c36f6cf22b8
quatquatt <78693624+quatquatt@users.noreply.github.com>
2023-10-18 01:31:01 +0000

    v0.3
    Changelog
    - Implemented code repeating based on user input
    - Optimized small areas of code

--- Main.java
+++ Main.java
@@ -4,23 +4,38 @@ class Main {
   public static void main(String[] args) {
 	  // Initialize variables here
 	  Scanner myObj = new Scanner(System.in); 
-	  int firstNumber = getIntegerInput("Tell me your first number. It has to be an integer.", myObj);
-	  int secondNumber = getIntegerInput("Now, tell me another integer.", myObj);
-	  char operator = getOperatorInput("Finally, tell me what operation you want to use. Your options are +, -, *, /, and %.", myObj);
-	  int result; 
-	  switch (operator) {
-		  case '+':
-			  result = MyMath.add(firstNumber, secondNumber);
-		  case '-':
-			  result = MyMath.subtract(firstNumber, secondNumber);
-		  case '*':
-			  result = MyMath.multiply(firstNumber, secondNumber);
-		  case '/':
-			  result = MyMath.divide(firstNumber, secondNumber);
-		  case '%':
-			  result = MyMath.modulo(firstNumber, secondNumber);
-	  }
-	  System.out.println("Your answer is " + result);
+	  int firstNumber, secondNumber, result;
+	  char operator;
+	  char goAgain = 'y';
+	  
+	  while (goAgain == 'y')
+		 {
+			  firstNumber = getIntegerInput("Tell me your first number. It has to be an integer.", myObj);
+			  secondNumber = getIntegerInput("Now, tell me another integer.", myObj);
+			  operator = getOperatorInput("Finally, tell me what operation you want to use. Your options are +, -, *, /, and %.", myObj);
+			  
+			  switch (operator) {
+				  case '+':
+					  result = MyMath.add(firstNumber, secondNumber);
+					  break;
+				  case '-':
+					  result = MyMath.subtract(firstNumber, secondNumber);
+					  break;
+				  case '*':
+					  result = MyMath.multiply(firstNumber, secondNumber);
+					  break;
+				  case '/':
+					  result = MyMath.divide(firstNumber, secondNumber);
+					  break;
+				  case '%':
+					  result = MyMath.modulo(firstNumber, secondNumber);
+					  break;
+				  default:
+					  result = -1; // This should never happen
+			  }
+			  System.out.println("Your answer is " + result);
+			  goAgain = getCharInput("Would you like to go again? Type \"y\" for yes, and \"n\" for no.", myObj);
+		  }
   }
 
 
@@ -49,5 +64,16 @@ class Main {
 			o = scanner.nextLine().charAt(0);
 		}
 		return o;
+	  }
+	public static char getCharInput(String message, Scanner scanner) {
+		System.out.println(message);
+		char yn = scanner.next().charAt(0);
+		while (yn != 'y'
+			   && yn != 'n')
+		{
+			System.out.println("You didn't say y or n! Please try again.");
+			yn = scanner.nextLine().charAt(0);
+		}
+		return yn;
 	  }
 }
